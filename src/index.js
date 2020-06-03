0c27d51644e63ed69fa7672122edfeadc457f1be
furkanmavili <furkanmavili@outlook.com>
2020-06-03 16:02:37 +0300

    Finish random word generate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-let words =
-  "lorem ipsum dolor sit amet consectetur adipisicing elit perferendis labore ue sint";
-swords = words.split("");
-
+import word from "./words";
+console.log(word.mostCommon[2]);
 const keyMaps = {
   a: 65,
   b: 66,
@@ -31,32 +29,23 @@ const keyMaps = {
   z: 90,
   " ": 32,
 };
-
-const values = Object.values(keyMaps);
-const keys = Object.keys(keyMaps);
 let wordArea = document.querySelector(".words");
-let key;
-swords.forEach((e) => {
-  for (let i = 0; i < e.length; i++) {
-    let span = document.createElement("span");
-    span.classList.add("letter");
-    if (e[i].length === 1 && e[i].match(/[a-z]/i)) {
-      span.textContent = e[i];
-    } else {
-      span.innerHTML = "␣";
-      span.classList.add("space");
-    }
-    wordArea.appendChild(span);
-  }
-});
+
+let words = "";
+let swords = [];
+let alleters;
+draw();
 let current = 0;
-let alleters = document.querySelectorAll(".letter");
 let wrong = 0;
 alleters[current].classList.add("cursor");
 window.addEventListener("click", isFocused);
+
+// curtime end finTime for calculate wpm speed
 let curTime;
 let finTime;
+
 let activate = document.querySelector(".activate");
+// checks wordArea is focused or not, if so start keyboard event.
 function isFocused(e) {
   if (Array.from(e.target.classList).includes("words")) {
     curTime = "";
@@ -64,25 +53,33 @@ function isFocused(e) {
     activate.style.display = "none";
     wordArea.classList.remove("words-off");
     curTime = new Date();
+    show();
     window.addEventListener("keyup", start);
   } else {
     curTime = "";
     finTime = "";
+    current = 0;
+    wrong = 0;
     activate.style.display = "block";
     wordArea.classList.add("words-off");
+    reset();
     window.removeEventListener("keyup", start);
   }
 }
 
+// compares e.keyCode with expected letter
 function start(e) {
-  console.log(curTime.getMinutes());
+  console.log(e.keyCode);
   if (keyMaps[words.charAt(current)] == e.keyCode) {
     alleters[current].classList.remove("cursor");
+    alleters[current].classList.add("correct");
     current++;
     if (current == words.length) {
       finTime = new Date();
       alert(`You have finished with ${wrong} mistakes`);
+      clear();
       calculate();
+      draw();
       window.removeEventListener("keyup", start);
     }
     alleters[current].classList.add("cursor");
@@ -92,6 +89,57 @@ function start(e) {
   }
 }
 
+function generate() {
+  swords = [];
+  for (let i = 0; i < 20; i++) {
+    let random = Math.round(Math.random() * 999);
+    swords.push(word.mostCommon[random]);
+    swords.push(" ");
+  }
+  words = swords.join("");
+  console.log(words);
+}
+
+function draw() {
+  generate();
+  console.log(words);
+  console.log(swords);
+  swords.forEach((e) => {
+    for (let i = 0; i < e.length; i++) {
+      let span = document.createElement("span");
+      span.classList.add("letter");
+      if (e[i].length === 1 && e[i].match(/[a-z]/i)) {
+        span.textContent = e[i];
+      } else {
+        span.innerHTML = "␣";
+        span.classList.add("space");
+      }
+      wordArea.appendChild(span);
+    }
+  });
+  alleters = document.querySelectorAll(".letter");
+}
+// when typing is end, clear the screen
+function clear() {
+  current = 0;
+  wrong = 0;
+  while (wordArea.firstChild) {
+    wordArea.lastChild.remove();
+  }
+}
+// if wordArea focused, opacity = 1
+function show() {
+  alleters.forEach((el) => {
+    el.style.opacity = "1";
+  });
+}
+// if wordArea not focused, opacity = 0.5
+function reset() {
+  alleters.forEach((el) => {
+    el.style.opacity = "0.5";
+  });
+}
+// calculates wpm speed. words/min
 function calculate() {
   console.log(curTime);
   console.log(finTime);
